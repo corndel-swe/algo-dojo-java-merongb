bf9bb7774f8fca88e848f289622ebf78ec5e6fc9
Meron G <merong@Merons-MacBook-Air.local>
2025-07-24 16:11:15 +0100

    Started on workshop

--- src/main/java/com/corndel/two_pointers/array_reverser/ArrayReverser.java
+++ src/main/java/com/corndel/two_pointers/array_reverser/ArrayReverser.java
@@ -2,12 +2,26 @@ package com.corndel.two_pointers.array_reverser;
 
 public class ArrayReverser {
     public static int[] reverseBruteForce(int[] arr) {
-        return new int[0];
+        int[] reverse  = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reverse[i] = arr[arr.length - i - 1];
+        }
+        return reverse;
     }
 
 
     public static int[] reverseTwoPointers(int[] arr) {
-        return new int[0];
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            right--;
+            left++;
+        }
+        return arr;
     }
 
 }
--- src/main/java/com/corndel/two_pointers/pair_sum/PairSum.java
+++ src/main/java/com/corndel/two_pointers/pair_sum/PairSum.java
@@ -2,11 +2,31 @@ package com.corndel.two_pointers.pair_sum;
 
 public class PairSum {
     public static boolean hasPairSumBruteForce(int[] arr, int target) {
+
+        for  (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
 
     public static boolean hasPairSumTwoPointers(int[] arr, int target) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            if (arr[i] + arr[j] == target) {
+                return true;
+            }   else if (arr[i] + arr[j] > target) {
+                j--;
+            }   else if (arr[i] + arr[j] < target) {
+                i++;
+            }
+        }
         return false;
     }
 }
